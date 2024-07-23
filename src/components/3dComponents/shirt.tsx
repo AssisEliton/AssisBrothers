7728d4f95f311a981b09a2ec7eedb9e997c85b14
Eliton Lucas Lima de Assis <eliton.assis@cieepr.org.br>
2024-07-23 17:50:40 -0300

    ajustes

--- src/components/3dComponents/shirt.tsx
+++ src/components/3dComponents/shirt.tsx
@@ -1,33 +1,22 @@
 import React, { useEffect, useRef } from "react";
-import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber';
-import { Mesh, PerspectiveCamera, TextureLoader, Vector3 } from "three";
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { PerspectiveCamera } from "three";
 import { Box, OrbitControls, useGLTF } from '@react-three/drei';
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { Object3D } from 'three';
+import { Mesh } from 'three';
 
-
-const ShirtModel: React.FC = () => {
-  const gltf = useGLTF('/models/t_shirt.glb');
-
-  return <primitive object={gltf.scene} />;
-};
 
 
 function CameraAdjuster() {
   const { camera, size } = useThree();
   const initialSetup = useRef(false);
-   console.log(camera)
+
   const updateCamera = () => {
     const { width, height } = size;
-
-    // Ajusta a posição da câmera e a orientação
-    (camera as PerspectiveCamera).aspect = width / height; // Proporção do Canvas
-
-    camera.position.set(width / 2, height / 2, Math.max(width, height) * 1.5);
-    camera.lookAt(width / 2, height / 2, 0);
+    (camera as PerspectiveCamera).aspect = width / height;
+    camera.position.set(0, 0, 5); // Posiciona a câmera em z
+    camera.lookAt(0, 0, 0); // Aponta para o centro do canvas
     camera.updateProjectionMatrix();
   };
-
 
   useFrame(() => {
     if (!initialSetup.current) {
@@ -37,31 +26,57 @@ function CameraAdjuster() {
   });
 
   useEffect(() => {
-    // Atualiza a câmera quando a janela é redimensionada
     window.addEventListener('resize', updateCamera);
-    updateCamera(); // Atualiza a câmera na montagem
+    updateCamera();
     return () => window.removeEventListener('resize', updateCamera);
   }, [size, camera]);
 
   return null;
 }
 
-function CenteredModel() {
+
+const ShirtModel: React.FC = () => {
+  const gltf = useGLTF('/models/t_shirt.glb');
+  const modelRef = useRef<Mesh>(null);
+  const planeRef = useRef<Mesh>(null);
+  const { size } = useThree();
+
+  useFrame(() => {
+    if (modelRef.current) {
+      const { width, height } = size;
+      modelRef.current.position.set(0, -11.3, 0); // Posiciona o modelo ao centro do canvas
+      modelRef.current.scale.set(8.5,8.5,8.5); // Ajuste a escala conforme necessário
+      modelRef.current.rotation.set(0,0.5,0);
+    }
+  });
+
   return (
-    <Box args={[1, 1, 1]} position={[0, 0, 0]}>
-      <meshStandardMaterial color="orange" />
-    </Box>
+    <>
+      <primitive object={gltf.scene} ref={modelRef} />
+      <mesh ref={planeRef}>
+        <meshBasicMaterial color="green" />
+      </mesh>
+    </>
   );
-}
+};
 
 function Shirt() {
   return (
-    <Canvas>
+    <Canvas
+      gl={{ antialias: true }}
+      style={{ background: '#000000', minHeight: '50vh', height: '50vh', width: '100%' }} // Canvas com altura de 50vh e largura 100%
+    >
       <ambientLight intensity={0.5} />
       <directionalLight intensity={0.5} position={[1, 1, 1]} />
-      <OrbitControls />
+      <OrbitControls
+        maxPolarAngle={Math.PI / 2} // Limita a rotação para não ultrapassar o eixo X
+        minPolarAngle={Math.PI / 2} // Limita a rotação para não ultrapassar o eixo X
+        enableRotate // Habilita a rotação
+        enableZoom={false} // Desabilita o zoom, se desejado
+        enablePan={false} // Desabilita o pan, se desejado
+      />
       <CameraAdjuster />
-      <CenteredModel />
+      <ShirtModel />
     </Canvas>
   );
 }
